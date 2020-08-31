de83bf8e79a036b2ab444aa5e9b43499bcc77a8b
Cataline Baert <snuf_kin@yahoo.com>
2020-08-31 10:12:47 +0200

    install axios package and implement search with it

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import request from 'request';
+// import request from 'request';
+import axios from "axios";
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,20 +9,33 @@ app.get('/', (req, res) => {
   res.render('search');
 });
 
-app.get('/results', (req, res) => {
+app.get("/results", (req, res) => {
   const query = req.query.search;
   const url = `https://omdbapi.com/?apikey=thewdb&s=${query}`;
-  request(url, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        const data = JSON.parse(body);
-        res.render('results', {data: data});
-      } else {
-        console.log("Oups! Something went wrong.");
-        console.log("Error: ", error);
-      }
-    }
-  );
+  axios.get(url)
+    .then((response) => {
+      const data = response.data;
+      res.render('results', { data: data});
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 });
+
+// app.get('/results', (req, res) => {
+//   const query = req.query.search;
+//   const url = `https://omdbapi.com/?apikey=thewdb&s=${query}`;
+//   request(url, (error, response, body) => {
+//       if (!error && response.statusCode == 200) {
+//         const data = JSON.parse(body);
+//         res.render('results', {data: data});
+//       } else {
+//         console.log("Oups! Something went wrong.");
+//         console.log("Error: ", error);
+//       }
+//     }
+//   );
+// });
 
 app.listen(3000, () => {
   console.log('Movie App Server has started!');
